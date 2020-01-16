05eb098b7050898d522b639b48f017fd12d42731
Lesleytsai9 <e861k91677@gmail.com>
2020-01-16 22:54:32 +0800

    Initial Commit

---
+++ 0116HealthLayoutTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0116HealthLayoutTest
+//
+//  Created by 蔡家雯 on 2020/1/16.
+//  Copyright © 2020 lesley tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 0116HealthLayoutTestTests/_116HealthLayoutTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  _116HealthLayoutTestTests.swift
+//  0116HealthLayoutTestTests
+//
+//  Created by 蔡家雯 on 2020/1/16.
+//  Copyright © 2020 lesley tsai. All rights reserved.
+//
+
+import XCTest
+@testable import _116HealthLayoutTest
+
+class _116HealthLayoutTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
